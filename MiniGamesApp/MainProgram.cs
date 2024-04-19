8d7fe0a08a93cbb268ec3c92802a171f6c048e73
Joren Vandewalle <joren.vandewalle@gmail.com>
2024-04-19 23:12:50 +0200

    Add project files.

---
+++ MiniGamesApp/MainProgram.cs
@@ -0,0 +1,30 @@
+﻿using MiniGamesAppLibrary;
+
+namespace MiniGamesApp
+{
+    internal class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Welcome to the MiniGameApp.");
+                Console.WriteLine("Here you can choose and play a game. HAVE FUN!!!");
+                Console.Write("Choose a game:");
+
+                int gameNumber = Convert.ToInt32(Console.ReadLine());
+
+                switch (gameNumber)
+                {
+                    case 1:
+                        HigherLower higherLowerGame = new HigherLower();
+                        higherLowerGame.HigherLowerMain();
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid number");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ MiniGamesAppLibrary/HigherLower.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MiniGamesAppLibrary
+{
+    public class HigherLower
+    {
+        private int secretNumber;
+
+        public void HigherLowerMain()
+        {
+            Console.WriteLine("\nWelcome to the HigherLower game.");
+            Console.WriteLine("A random number from 1 to 50 will be generated.");
+
+            secretNumber = RandomGen(); // Generate secret number once
+
+            int yourNumber;
+            bool guessed = false;
+
+            while (!guessed)
+            {
+                Console.Write("Please make a guess: ");
+                yourNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (yourNumber > secretNumber)
+                {
+                    Console.WriteLine("\nToo high! Try again.");
+                }
+                else if (yourNumber < secretNumber)
+                {
+                    Console.WriteLine("\nToo low! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine($"\nYou guessed it!!! It was {secretNumber}");
+                    guessed = true; // Set guessed to true to exit the loop
+                }
+            }
+
+            Console.Write("\n1: Play again!  2: Back to menu  -> ");
+            int whatNext = Convert.ToInt32(Console.ReadLine());
+            switch (whatNext)
+            {
+                case 1: HigherLowerMain();
+                    break;
+                case 2:
+                    return;
+                default:
+                    Console.WriteLine("Something went wrong!?!?");
+                    break;
+            }
+        }
+
+        private int RandomGen()
+        {
+            Random random = new Random();
+            return random.Next(1, 51); // Generate a random number from 1 to 50
+        }
+    }
+}
